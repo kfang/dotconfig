ad237a533acd7ff815597077c50e70a14a28b6e3
Kevin Fang <hkr.fang@gmail.com>
2025-03-14 22:38:46 -0700

    add deno config

---
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -0,0 +1,21 @@
+local nvim_lsp = require("lspconfig")
+
+return {
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        denols = {
+          filetypes = { "typescript", "typescriptreact" },
+          root_dir = function(...)
+            return nvim_lsp.util.root_pattern("deno.jsonc", "deno.json")(...)
+          end,
+        },
+        vtsls = {
+          root_dir = nvim_lsp.util.root_pattern("package.json"),
+          single_file_support = false,
+        },
+      },
+    },
+  },
+}
